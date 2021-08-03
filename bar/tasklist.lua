c5d32c2f4ff2797ff64b481dcf32254791ba7a7e
Alex <hellosimon1103@hotmail.com>
2021-08-03 11:52:28 +0800

    bar: use fixed width for tasklist

--- bar/tasklist.lua
+++ bar/tasklist.lua
@@ -9,8 +9,8 @@ local dpi = xresources.apply_dpi
 local tasklist = function(s)
   local tasklist_buttons = gears.table.join(
     awful.button({}, 1, function(c)
-    if c == client.focus then c.minimized = true
-    else c:emit_signal("request::activate", "tasklist", {raise = true}) end
+        if c == client.focus then c.minimized = true
+        else c:emit_signal("request::activate", "tasklist", {raise = true}) end
     end),
     awful.button({}, 3, function() awful.menu.client_list({theme = {width = 250}}) end),
     awful.button({}, 4, function() awful.client.focus.byidx(1) end),
@@ -21,31 +21,35 @@ local tasklist = function(s)
     buttons = tasklist_buttons,
     style = {bg = beautiful.wibar_bg, shape = shapes.rrect(10)},
     layout = {
-        spacing = dpi(8),
-        layout = wibox.layout.fixed.horizontal
+      spacing = dpi(8),
+      layout = wibox.layout.fixed.horizontal
     },
     widget_template = {
+      {
         {
-            {
-                {
-                    awful.widget.clienticon,
-                    top = dpi(1),
-                    bottom = dpi(1),
-                    right = dpi(1),
-                    layout = wibox.container.margin
-                },
-                shapes.horizontal_pad(6),
-                {id = 'text_role', widget = wibox.widget.textbox},
-                layout = wibox.layout.fixed.horizontal
-            },
-            top = dpi(5),
-            bottom = dpi(5),
-            left = dpi(10),
-            right = dpi(10),
-            widget = wibox.container.margin
+          {
+            awful.widget.clienticon,
+            top = dpi(1),
+            bottom = dpi(1),
+            right = dpi(1),
+            layout = wibox.container.margin
+          },
+          shapes.horizontal_pad(6),
+          {
+            id = 'text_role',
+            widget = wibox.widget.textbox,
+            forced_width = dpi(100)
+          },
+          layout = wibox.layout.fixed.horizontal
         },
-        id = "background_role",
-        widget = wibox.container.background
+        top = dpi(5),
+        bottom = dpi(5),
+        left = dpi(10),
+        right = dpi(10),
+        widget = wibox.container.margin
+      },
+      id = "background_role",
+      widget = wibox.container.background
     }
 } end
 
